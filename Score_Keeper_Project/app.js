7fd9bf1c145ccf0970d5f16507f99fed26711554
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-01-26 21:19:21 -0600

    button stops counting up when the winningscore is reached

--- Score_Keeper_Project/app.js
+++ Score_Keeper_Project/app.js
@@ -1,22 +1,34 @@
+const p1Display = document.querySelector("#p1Display");
+const p2Display = document.querySelector("#p2Display");
+const p1 = document.querySelector("#player1");
+const p2 = document.querySelector("#player2");
+const reset = document.querySelector("#reset");
+const gameSelect = document.querySelectorAll("#game-select");
+
 let p1Score = 0;
 let p2Score = 0;
 
+let winningScore = 5;
+let isGameOver = false;
 
-const score = document.querySelector('#score');
-const gameSelect = document.querySelectorAll('#game-select');
-
-const p1 = document.querySelector('#player1');
-const p2 = document.querySelector('#player2');
-const reset = document.querySelector('#reset');
-
-
-p1.addEventListener('click', (e) => {
+p1.addEventListener("click", () => {
+  if (!isGameOver) {
     p1Score++;
-    score.innerHTML = `${p1Score} to ${p2Score}`;
-})
+    if (p1Score === winningScore) {
+      isGameOver = true;
+    }
+    p1Display.textContent = p1Score;
+  }
+});
 
-p2.addEventListener('click', (e) => {
-    p2Score++;
-    score.innerHTML = `${p1Score} to ${p2Score}`;
-})
 
+p2.addEventListener("click", () => {
+    if (!isGameOver) {
+      p2Score++;
+      if (p2Score === winningScore) {
+        isGameOver = true;
+      }
+      p2Display.textContent = p2Score;
+    }
+  });
+  
