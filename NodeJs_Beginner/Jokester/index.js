c19a316e96d9d721afcaf83651d0d93d936f2cc4
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-02-09 00:12:55 -0600

    global npm installs cowsay

--- NodeJs_Beginner/Jokester/index.js
+++ NodeJs_Beginner/Jokester/index.js
@@ -1,5 +1,11 @@
 const jokes = require('give-me-a-joke');
 const color = require('colors');
-
+const cowsay = require('cowsay')
 
 jokes.getRandomDadJoke(joke => console.log(joke.rainbow))
+
+console.log(cowsay.say({
+    text:' grazing in the browser'.rainbow,
+    e: '- -',
+    T: ';;'
+}));
