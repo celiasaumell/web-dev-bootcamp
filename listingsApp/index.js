a89e97e6be06be94b45f2db3bcd0ce32f60dddb3
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-02-14 23:07:58 -0600

    includes separate js file with array of listings
    introduced ejs and used middleware to post data in req.body,

--- listingsApp/index.js
+++ listingsApp/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const listings = require("./list")
 
 const app = express();
 const port = 3000;
 
-app.get("/listings", (req, res) => {
-    res.send("It's working!")
-})
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+
+app.get("/listings", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Listing App listening at http://localhost:${port}`);
