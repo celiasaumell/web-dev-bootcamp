082fab49fc6dde5f5e0c45df87cbeced5670c55f
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-02-15 17:07:01 -0600

    form to create new listing

--- listingsApp/index.js
+++ listingsApp/index.js
@@ -11,14 +11,25 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.use(express.static(path.join(__dirname, "public")))
+
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get("/listings", (req, res) => {
-  res.render("listings/index", { listings });
+  res.render("listings", { listings });
 });
 
+app.post("/listings", (req, res) => {
+  const {title, host, price, description} = req.body;
+  let {imgSrc} = req.body;
+  imgSrc = `images/${imgSrc}`;
+  listings.push({title, host, description, imgSrc, price, id: uuidv4()})
+  res.redirect("listings");
+})
 
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new")
+})
 
 app.listen(port, () => {
   console.log(`Listing App listening at http://localhost:${port}`);
