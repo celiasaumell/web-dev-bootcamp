497a67d714d7822a2333591473cae7d10d712d5c
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-02-15 14:21:47 -0600

    added bootstrap library and passed in listings to index.ejs

--- listingsApp/index.js
+++ listingsApp/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
-const listings = require("./list")
+const listings = require("./list");
 
 const app = express();
 const port = 3000;
@@ -10,12 +10,15 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+app.use(express.static(path.join(__dirname, "public")))
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+app.get("/listings", (req, res) => {
+  res.render("listings/index", { listings });
+});
 
 
-app.get("/listings", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Listing App listening at http://localhost:${port}`);
