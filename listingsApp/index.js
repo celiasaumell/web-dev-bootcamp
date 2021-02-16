4c5d5aa27eb5b78a409fd011bd0ed62be0af8d1a
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-02-15 18:39:42 -0600

    delete a post using method override

--- listingsApp/index.js
+++ listingsApp/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
-const listings = require("./list");
+let listings = require("./list");
+const methodOverride = require("method-override");
 
 const app = express();
 const port = 3000;
@@ -10,25 +11,38 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get("/listings", (req, res) => {
-  res.render("listings", { listings });
+  res.render("listings/index", { listings });
 });
 
 app.post("/listings", (req, res) => {
-  const {title, host, price, description} = req.body;
-  let {imgSrc} = req.body;
-  imgSrc = `images/${imgSrc}`;
-  listings.push({title, host, description, imgSrc, price, id: uuidv4()})
-  res.redirect("listings");
-})
+  const { title, host, price, description } = req.body;
+  let { imgSrc } = req.body;
+  imgSrc = `/images/${imgSrc}`;
+  listings.push({ title, host, description, imgSrc, price, id: uuidv4() });
+  res.redirect("listings/index");
+});
 
 app.get("/listings/new", (req, res) => {
-  res.render("listings/new")
+  res.render("listings/new");
+});
+
+app.get("/listings/:id", (req, res) => {
+  const { id } = req.params;
+  const listing = listings.find((l) => l.id === id);
+  res.render("listings/show", { listing });
+});
+
+app.delete("/listings/:id", (req, res) => {
+  const { id } = req.params;
+  listings = listings.filter((l) => l.id !== id);
+  res.redirect("/listings");
 })
 
 app.listen(port, () => {
