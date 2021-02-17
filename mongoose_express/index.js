34dd16fb7cc00686b7bddce5bd2bcb2c6cb1083f
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-02-16 20:42:09 -0600

    create a product and edit existing products

--- mongoose_express/index.js
+++ mongoose_express/index.js
@@ -2,17 +2,25 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const Product = require("./models/product");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("products/index", { products });
+});
+
+app.get("/products/new", async (req, res) => {
+  res.render("products/new");
 });
 
 app.get("/products/:id", async (req, res) => {
@@ -21,11 +29,33 @@ app.get("/products/:id", async (req, res) => {
   res.render("products/show", { product });
 });
 
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true
+  });
+  res.redirect(`/products/${updatedProduct._id}`);
+});
+
 app.listen(3000, () => {
   console.log("app is listening on port 3000");
 });
 
 const { application } = require("express");
+const { findByIdAndUpdate } = require("./models/product");
 mongoose
   .connect("mongodb://localhost:27017/farmStand", {
     useNewUrlParser: true,
