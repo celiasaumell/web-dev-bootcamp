857bd9e6893fcf46cbab24a087653170b0883bb5
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-02-25 22:53:52 -0600

    deletes reviews from reviews db of a deleted campground
    using middleware

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -23,8 +23,16 @@ const CampgroundSchema = new Schema({
     {
       type: Schema.Types.ObjectId,
       ref: "Review",
-    }
+    },
   ],
 });
-
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if(doc){
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews
+      }
+    })
+  }
+});
 module.exports = mongoose.model("Campground", CampgroundSchema);
