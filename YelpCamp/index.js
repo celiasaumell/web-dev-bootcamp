5855bdc43c0c56494546cc92547ba000be5159ab
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-03-06 01:15:24 -0600

    client side form validation moved to public folder

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -16,6 +16,7 @@ mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 const db = mongoose.connection;
