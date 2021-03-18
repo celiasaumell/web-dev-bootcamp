409ffbdc0d519eefd64c7660a04bb9fa94047efb
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-03-17 20:48:07 -0500

    delete from cloudinary

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require("../models/campground");
+const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -49,10 +50,16 @@ module.exports.editCampground = async (req, res) => {
   const campground = await Campground.findByIdAndUpdate(req.params.id, {
     ...req.body.campground,
   });
-  const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }))
+  const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
   campground.images.push(...imgs);
   await campground.save();
-  console.log(req.body)
+  if (req.body.deleteImages) {
+    for(let filename of req.body.deleteImages){
+      await cloudinary.uploader.destroy(filename)
+    }
+    await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+    console.log(campground);
+  }
   req.flash("success", "Successfully updated campground!");
   res.redirect(`/campgrounds/${campground._id}`);
 };
