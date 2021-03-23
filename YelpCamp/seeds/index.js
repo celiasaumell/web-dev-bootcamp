f99cfaaf7078ee8e3ff29c85a156ea068427ed84
Celia Saumell <56240891+celiasaumell@users.noreply.github.com>
2021-03-22 20:27:19 -0500

    cluster map

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -21,7 +21,7 @@ places[Math.floor(Math.random() * places.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 100; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
@@ -30,12 +30,16 @@ const seedDB = async () => {
       title: `${sample(descriptors)} ${sample(places)}`,
       images: [
         {
-        },
-        {
-        },
+          url: 'https://res.cloudinary.com/dgrqemg3h/image/upload/v1616031793/YelpCamp/lcvacuigavsiks92epzr.webp',
+          filename: 'YelpCamp/lcvacuigavsiks92epzr'
+        }
       ],
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna neque viverra justo nec. Purus in massa tempor nec. Sit amet consectetur adipiscing elit. Id aliquet risus feugiat in. Nibh tellus molestie nunc non blandit massa enim nec. Aliquam vestibulum morbi blandit cursus risus at ultrices mi. Turpis egestas maecenas pharetra convallis posuere morbi leo urna. Dictum sit amet justo donec enim diam vulputate ut. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Tortor aliquam nulla facilisi cras fermentum odio. Sed turpis tincidunt id aliquet risus feugiat. Ornare arcu dui vivamus arcu felis bibendum. Blandit turpis cursus in hac habitasse platea dictumst quisque sagittis. Volutpat sed cras ornare arcu dui vivamus. Mus mauris vitae ultricies leo integer malesuada nunc vel. Sed cras ornare arcu dui vivamus arcu felis bibendum ut. Quis vel eros donec ac. Consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat.",  
       price,
+      geometry: {
+        type: "Point",
+        coordinates: [cities[random1000].longitude, cities[random1000].latitude]
+      }
     });
     await camp.save();
   }
